388d67df9018e996653c81ce075fa878c0d98596
brian <bgereke@utexas.edu>
2020-10-20 17:19:04 -0700

    add binary tree inorder traversal

--- trees_and_graphs/binary_tree_inorder_traversal.py
+++ trees_and_graphs/binary_tree_inorder_traversal.py
@@ -76,3 +76,10 @@ class Solution:
             return traverse(root)
         else:
             return []
+
+# one line recursive solution
+class Solution:
+    def preorderTraversal(self, root: TreeNode) -> List[int]:
+        if not root:
+            return []
+        return self.inorderTraversal(root.left) + [root.val] +  self.inorderTraversal(root.right)
